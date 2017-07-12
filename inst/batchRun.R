3f286f0f8b49eeacef9de03c4b662c93648605ac
mike <mike.mcmahon@dfo-mpo.gc.ca>
2017-07-12 16:00:50 -0300

    fixes

--- inst/batchRun.R
+++ inst/batchRun.R
@@ -1,10 +1,6 @@
-batchRun <-function(thePath = file.path("C:","DFO-MPO","PORTSAMPLING"),
-                    fn.oracle.username = "<your_oracle_username>",
-                    fn.oracle.password = "<your_oracle_password>",
-                    fn.oracle.dsn = "<your_oracle_dsn>"){
+args=(commandArgs(TRUE))
 require(bio.portsampling)
-makeHailInRpt(thePath = thePath,
-              fn.oracle.username = fn.oracle.username,
-              fn.oracle.password = fn.oracle.password,
-              fn.oracle.dsn = fn.oracle.dsn)
-  }
+makeHailInRpt(thePath = "C:/DFO-MPO/PORTSAMPLING",
+              fn.oracle.username = args[1],
+              fn.oracle.password = args[2],
+              fn.oracle.dsn = args[3])
