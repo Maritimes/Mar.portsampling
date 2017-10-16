00345898dca77bf95b44db9f8b22ab944e74ad8b
Mike McMahon <mike.mcmahon@dfo-mpo.gc.ca>
2017-10-16 13:42:41 -0300

    Update example.bat

--- inst/example.bat
+++ inst/example.bat
@@ -4,8 +4,8 @@ REM can just save this file on your desktop, and when you double-click it, the
 REM reports will be generated.
 REM
 REM You can generate one of these automatically via
-REM bio.portsampling::makePortSamplingBat()
+REM portsampling::makePortSamplingBat()
 REM
 REM <Path to your i386\Rscript.exe file> <Path to the scipt you want to run> <Parameters>
-"C:\Program Files\R\R-3.4.0\bin\i386\Rscript.exe" "C:\git\Maritimes\bio.portsampling\inst\batchRun.R" <username> <password> <dsn>
+"C:\Program Files\R\R-3.4.0\bin\i386\Rscript.exe" "C:\git\Maritimes\portsampling\inst\batchRun.R" <username> <password> <dsn>
 PAUSE
